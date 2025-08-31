f884fe4ad6574d67a7004d965516ee419b23c39a
Enceka <enceka@yeah.net>
2025-08-31 08:47:39 +0800

    pearl: blob: Fixup error: Unresolved symbol: _ZN7android13SetUpMinijailERKNSt3__112basic_stringIcNS0_11char_traitsIcEENS0_9allocatorIcEEEES8_ for android.hardware.media.c2@1.2-mediatek

--- extract-files.py
+++ extract-files.py
@@ -65,9 +65,15 @@ blob_fixups: blob_fixups_user_type = {
 	'vendor/bin/hw/vendor.xiaomi.hardware.vibratorfeature.service': blob_fixup()
     .replace_needed('android.hardware.vibrator-V1-ndk_platform.so','android.hardware.vibrator-V1-ndk.so'),
 
+    ('vendor/bin/hw/android.hardware.media.c2@1.2-mediatek'
+    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b'): blob_fixup()
+        .add_needed('libprocessgroup.so'),
+
     ('vendor/lib64/mt6895/libcam.hal3a.so',
      'vendor/lib64/mt6895/libcam.hal3a.ctrl.so',
-     'vendor/lib64/mt6895/libmtkcam_request_requlator.so'): blob_fixup()
+     'vendor/lib64/mt6895/libmtkcam_request_requlator.so',
+     'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek',
+     'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b'): blob_fixup()
         .add_needed('libprocessgroup_shim.so'),
 
     ('vendor/lib64/lib3a.ae.pipe.so',
